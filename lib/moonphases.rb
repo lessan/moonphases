adebe4e65ccd8223ba8fe53394d54199c302ecdf
Curtis Lacy <curtis.lacy@grgcomponents.com>
2012-04-19 20:59:42 -0400

    Added URL creation for AD years.

--- lib/moonphases.rb
+++ lib/moonphases.rb
@@ -1,5 +1,17 @@
 class MoonPhases
-  def self.spoutBack( message )
-    message + " is the message I'm sending back!"
+  def self.lookupURL( year )
+    "http://eclipse.gsfc.nasa.gov/phase/phases" + paddedString( (( year - 1 )/ 100 ) * 100 + 1 ) + ".html"
+  end
+  
+  def self.paddedString( value )
+    if value < 10
+      "000" + value.to_s
+    elsif value < 100
+      "00" + value.to_s
+    elsif value < 1000
+      "0" + value.to_s
+    else
+      value.to_s
+    end
   end
 end
--- test/test_moonphases.rb
+++ test/test_moonphases.rb
@@ -2,7 +2,23 @@ require 'test/unit'
 require 'moonphases'
 
 class MoonPhasesTest < Test::Unit::TestCase
-  def test_spoutBack
-    assert_equal "myMessage is the message I'm sending back!", MoonPhases.spoutBack( "myMessage" )
+  def test_AD_Years
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0001.html", MoonPhases.lookupURL( 1 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0001.html", MoonPhases.lookupURL( 100 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0101.html", MoonPhases.lookupURL( 101 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0101.html", MoonPhases.lookupURL( 200 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0201.html", MoonPhases.lookupURL( 201 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0201.html", MoonPhases.lookupURL( 300 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0301.html", MoonPhases.lookupURL( 301 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases0901.html", MoonPhases.lookupURL( 1000 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases1001.html", MoonPhases.lookupURL( 1001 )
+    assert_equal "http://eclipse.gsfc.nasa.gov/phase/phases2001.html", MoonPhases.lookupURL( 2012 )
+  end
+  
+  def test_padding
+    assert_equal "0001", MoonPhases.paddedString( 1 )
+    assert_equal "0010", MoonPhases.paddedString( 10 )
+    assert_equal "0100", MoonPhases.paddedString( 100 )
+    assert_equal "1000", MoonPhases.paddedString( 1000 )
   end
 end
